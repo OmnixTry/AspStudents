00b68111c1e0f848d7c6c0f96f14e2b4e51e364e
OmnixTry <Omnix.try@gmail.com>
2021-01-27 12:55:22 +0200

    add IDummyData and change DI

--- API/StudApi/Startup.cs
+++ API/StudApi/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using StudApi.Models.DummyData;
 
 namespace StudApi
 {
@@ -24,6 +25,8 @@ namespace StudApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddSingleton<IDummyData>(new DataSource());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
