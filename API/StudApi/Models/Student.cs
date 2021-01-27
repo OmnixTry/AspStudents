1b43da96c968937a23100c17efe75d91c3865835
OmnixTry <Omnix.try@gmail.com>
2021-01-27 11:58:40 +0200

    Add Student properties

--- API/StudApi/Models/Student.cs
+++ API/StudApi/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,14 @@ namespace StudApi.Models
 {
     public class Student
     {
+        [Required]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Patronimic { get; set; }
+
     }
 }
