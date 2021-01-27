dec768098189ab4ea579e077c26e2b2a75b3e379
OmnixTry <Omnix.try@gmail.com>
2021-01-27 12:02:08 +0200

    remove unnecessaty isongs from model class files

--- API/StudApi/Models/Student.cs
+++ API/StudApi/Models/Student.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace StudApi.Models
 {
@@ -16,6 +12,5 @@ namespace StudApi.Models
         public string Surname { get; set; }
 
         public string Patronimic { get; set; }
-
     }
 }
---
+++ API/StudApi/Models/Subject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudApi.Models
+{
+    public class Subject
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int Room { get; set; }
+
+
+    }
+}
