8c99989aff432bbb1955dd2df9de149511bfd8a4
OmnixTry <Omnix.try@gmail.com>
2021-01-27 12:22:59 +0200

    Add studentSubject joining class

---
+++ API/StudApi/Models/StudentSubject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudApi.Models
+{
+    public class StudentSubject
+    {
+        [Required]
+        public int StudentId { get; set; } 
+
+        [Required]
+        public int SubjectId { get; set; }
+
+    }
+}
